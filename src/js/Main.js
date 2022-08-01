0f6852606730415072113317ba18cbdbe2417b8a
Ryan Brayan <ryanbrayanf@gmail.com>
2022-08-01 12:02:00 -0300

    Puxando com Enter agora

--- src/js/Main.js
+++ src/js/Main.js
@@ -25,3 +25,20 @@ cep.addEventListener('blur', (evento)=>{
     } )
     .catch(e => console.log('Deu erro '+ e,message  ))
 })
+
+document.addEventListener('keypress', function(e){
+    if(e.key == 'Enter'){
+        let search = cep.value.replace('-', '')
+        const options = {
+            method: 'GET', 
+            mode: 'cors',
+            cached: 'default'  
+        }
+    
+        fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+        .then( response => {response.json()
+            .then(data => showData(data))
+        } )
+        .catch(e => console.log('Deu erro '+ e,message  ))
+    }
+ }, false);
